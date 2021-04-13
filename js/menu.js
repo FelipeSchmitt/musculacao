06b25cd43a23df49eec78b7f19db97b06e1a6581
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2021-04-13 18:03:55 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 function menu(){
     var menu_img = document.getElementById("menu")
+    var content = document.getElementById("content")
     img = document.createElement("img")
     img.src="../image/menu.png"
     menu_img.appendChild(img)
@@ -10,6 +11,22 @@ function menu_show(){
         topics.visibility="visible"
     }else{
         topics.visibility="hidden"
+    }
+
+    if (window.matchMedia("(max-width:60em)").matches) {
+        if (content.style.marginTop=="0px"){
+            content.style.marginTop="250px"
+        }else{
+            content.style.marginTop="0"
+        }
+
+    }
+    if (window.matchMedia("(min-width:60em)").matches) {
+        if (content.style.marginTop=="0px"){
+            content.style.marginTop="50px"
+        }else{
+            content.style.marginTop="0"
+        }
     }
     
 }
