9d140b1737abe35c83b9dcf36a4cfcebc0d3585e
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2021-04-15 11:05:27 -0300

    Add files via upload

--- js/bf_calculator.js
+++ js/bf_calculator.js
@@ -1,1 +1,13 @@
-s
+function bf_calculator(){
+    result = document.getElementById("result")
+    gender = document.getElementById("gender").value
+    height = document.getElementById("height").value
+    circumference = document.getElementById("circumference").value
+    cost_masc = 63 - (20 * height / circumference)
+    cost_fem = 76 - (20 * height / circumference)
+    if(gender == "F"){
+        result.innerHTML= `Seu percentual de gordura é de ` + cost_fem.toFixed(1) + `%`
+    }else{
+        result.innerHTML= `Seu percentual de gordura é de ` + cost_masc.toFixed(1) + `%`
+    }
+}
